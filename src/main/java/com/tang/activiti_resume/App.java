7749dae6089a5fb709f094f19516faa8dd28a908
tang <tang@localhost.localdomain>
2018-04-19 09:09:53 +0800

    initial project...

---
+++ src/main/java/com/tang/activiti_resume/App.java
@@ -0,0 +1,13 @@
+package com.tang.activiti_resume;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
